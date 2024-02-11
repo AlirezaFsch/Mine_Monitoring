4fadda505682cbca569b292734e7969015f01305
Alireza fsch <alireza.richard@yahoo.com>
2024-02-12 00:58:59 +0330

    commit 5

--- Mine_Monitoring/Controllers/StatisticsController.cs
+++ Mine_Monitoring/Controllers/StatisticsController.cs
@@ -1,48 +1,12 @@
-﻿// In StatisticsController.cs
+﻿using Microsoft.AspNetCore.Mvc;
 
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Mine_Monitoring.Controllers
+namespace YourNamespace.Controllers
 {
     public class StatisticsController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
-        }
-
-        // Action to display buttons for different Excel files
-        public IActionResult ExcelFiles()
-        {
-            return View();
-        }
-
-        // Action to fetch and display preview of an Excel file
-        public IActionResult PreviewExcel(string fileName)
-        {
-            // Get the path to the Excel file based on the file name
-            string filePath = Path.Combine("PathToYourExcelFilesDirectory", fileName);
-
-            // Read the Excel file and extract the first 10 rows
-            // You can use a library like EPPlus or ClosedXML to read Excel files
-            // For simplicity, assume we have a method ReadExcelFile that reads the file and returns the data as a list of strings
-            List<string> excelData = ReadExcelFile(filePath).Take(10).ToList();
-
-            return View(excelData);
-        }
-
-        // Method to read Excel file
-        private List<string> ReadExcelFile(string filePath)
-        {
-            // Code to read Excel file and extract data
-            // Implement this method using a library like EPPlus or ClosedXML
-            // For demonstration purposes, assume it returns a list of strings
-            return new List<string>();
         }
     }
 }
